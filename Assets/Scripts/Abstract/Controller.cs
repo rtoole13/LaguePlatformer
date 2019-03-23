c43ccc0804675c5e1f9154fbef8253b37ac30387
Ryan <rtoole13@gmail.com>
2019-03-23 11:15:25 -0400

    forgot to push controller.

---
+++ Assets/Scripts/Abstract/Controller.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Controller : MonoBehaviour
+{
+    public abstract void Move(Vector3 dist);
+}
